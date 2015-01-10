811baed47aa3cfb894360e5ccfd38b1b44d1d610
John Hofrichter <you@example.com>
2015-01-09 23:40:33 -0500

    home styling tweaks

--- app/core/router.js
+++ app/core/router.js
@@ -4,13 +4,13 @@ popa.config(['$routeProvider', function ($routeProvider) {
 
     // Home
     when('/', {
-      templateUrl    : 'views/home.html',
-      controller     : 'HomeCtrl'
+      templateUrl : 'views/home.html',
+      controller  : 'HomeCtrl'
     }).
 
     // 404
     otherwise({
-      templateUrl : 'views/404.html',
+      templateUrl : 'views/404.html'
     });
   }
 ]);
