57c0c490940834c8fa171b8cdcc4595d9e064a57
John Hofrichter <you@example.com>
2015-01-17 22:16:31 -0500

    footer updates

--- app/components/footer/footer_ctrl.js
+++ app/components/footer/footer_ctrl.js
@@ -1,5 +1,10 @@
 
 var headerCtrl = angular.module('popa').controller('FooterCtrl', ['$scope', '$location', 'Utils', function ($scope, $location, Utils) {
+  $scope.siteSrc = {
+    href : 'https://github.com/johnhof/popa',
+    text : 'View Source'
+  };
+
   $scope.sprites = Utils.spriteSet(['facebook','github', 'linkedin']);
 
   // make sure not to diplay on blacklisted pages
