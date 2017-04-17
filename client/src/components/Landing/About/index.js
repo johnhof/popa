2cb43d43f623d5e9125f3c08be0ffcdfaed82e7e
johnhof <john.hofrichter@gmail.com>
2017-04-17 00:47:21 -0700

    adding untracked files

---
+++ client/src/components/Landing/About/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+import Colors from '../../../assets/styles/colors';
+
+import BackgroundImg from '../../../assets/images/background.png';
+
+export default class About extends React.Component {
+  render() {
+    let about = {
+      backgroundColor: Colors.lightGray,
+      minHeight: '100%',
+      width: '100%',
+      color: Colors.darkGray,
+      background: `url(${BackgroundImg}) top left`
+    };
+
+    return (
+      <section id="about" style={about}>
+        <div>
+          <h1 style={{margin: "0"}}>About Ipsum</h1>
+        </div>
+      </section>
+    )
+  }
+}
