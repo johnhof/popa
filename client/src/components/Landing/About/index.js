51afbb5e4f191002764d011306b5b0c5f82825b0
johnhof <john.hofrichter@gmail.com>
2017-04-23 16:52:19 -0700

    more updates

--- client/src/components/Landing/About/index.js
+++ client/src/components/Landing/About/index.js
@@ -2,46 +2,58 @@ import React from 'react';
 
 import Colors from '../../../assets/styles/colors';
 
-import BackgroundImg from '../../../assets/images/background.png';
+import SizeEmitter from '../../../lib/SizeEmitter';
 
 import MyImg from '../../../assets/images/me_outline.png';
 
 export default class About extends React.Component {
   constructor (props) {
     super(props);
+    this.sizeEmitter = new SizeEmitter();
     this.styles = {
-      about: {
-        backgroundColor: Colors.lightGray,
-        maxHeight: '100vh',
-        color: Colors.darkGray,
-      },
       container: {
-        display: 'block',
-        height: 'auto',
-        width: '100%'
+        maxHeight: '100vh',
+        overflowY: 'hidden'
       },
-      textContainer: {
+      split: {
         height: '100%',
         width: '50%',
         display: 'inline-block'
       },
-      imageContainer: {
-        height: '100%',
-        width: '50%',
-        overflowY: 'hidden',
-        display: 'inline-block'
+      image: {
+        float: 'right',
+        margin: 'auto',
+        width: '75%'
       }
     };
+
+    this.sizeEmitter.on('small', () => {
+      this.styles.image.width = '100%';
+      this.styles.split.width = '100%';
+    });
+    this.sizeEmitter.on('medium', () => {
+      this.styles.image.width = '75%';
+      this.styles.split.width = '50%';
+    });
+    this.sizeEmitter.refresh();
   }
+
+  componentDidMount () {
+    window.addEventListener('resize', () => this.sizeEmitter.refresh(), false);
+  }
+
+  componentWillUnmount () {
+    window.removeEventListener('resize', () => this.sizeEmitter.refresh());
+  }
+
   render() {
     return (
-      <section id="about" style={this.styles.about}>
-        <div style={this.styles.container}>
-          <div style={this.styles.textContainer}>
-          </div>
-          <div style={this.styles.imageContainer}>
-            <img src={MyImg} style={{width: '100%'}}></img>
-          </div>
+      <section id="about" style={this.styles.container}>
+        <div style={this.styles.split}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </div>
+        <div style={this.styles.split}>
+          <img src={MyImg} style={this.styles.image}></img>
         </div>
       </section>
     )
